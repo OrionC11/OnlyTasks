2363517696ed5d121f6ddf5b6f103fb9f988609f
Orion Cannon <orion.m.cannon@gmail.com>
2024-05-30 19:22:00 -0700

    added basic code structure to client side and added some starter codes

---
+++ Client/src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="titleSection">
+        <h1>OnlyTasks</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/calender">Company Calender</Link>
+          </li>
+          <li>
+            <Link to="/completed">Completed Tasks</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/taskCreator">Task Creator</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ Client/src/components/taskCard/index.jsx
@@ -0,0 +1,28 @@
+import {useState} from react
+import {useMutation} from @apollo/client
+
+const TaskCard = () => {
+    return (
+        <div className= "taskCard">
+            <a className ="username" href="#"></a>
+            <div className="descSec">
+                <p className="desc"></p>
+            </div>
+            <div className="dateSec">
+                <p className="date"></p>
+            </div>
+            <div classname= "statusSec">
+                <input type="radio" name="statusGroup">Completed</input>
+                <input type="radio" name="statusGroup">In Progress</input>
+                <input type="radio" name="statusGroup">Not Complete</input>
+            </div>
+            <div className="prioritySec">
+                <input type="radio" name="priorityGroup">Low</input>
+                <input type="radio" name="priorityGroup">Medium</input>
+                <input type="radio" name="priorityGroup">High</input>
+            </div>
+            <button type="Submit">Submit</button>
+        </div>
+    )
+}
+export default TaskCard
