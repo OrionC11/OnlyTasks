7e9d824393b76c57ee26487f5e0d8ec463642345
Orion Cannon <orion.m.cannon@gmail.com>
2024-06-03 19:05:23 -0700

    added seed file and cleandb file and missing server side dependencies

--- Server/schemas/typeDefs.js
+++ Server/schemas/typeDefs.js
@@ -26,6 +26,47 @@ const typeDefs = `
         employee(username: String!): Employee
         tasks: [Task]
     }
+
+    type Mutation {
+        addEmployee(
+            username: String!, 
+            firstName: String!, 
+            lastName: String!, 
+            email: String!, 
+            password: String!
+        ): Employee
+        login(
+            username: String!, 
+            password: String!
+        ): Auth
+        addTask(
+            title: String!, 
+            description: String!, 
+            type: String!, 
+            status: String!
+        ): Task
+        updateEmployee(
+            _id: ID!, 
+            username: String, 
+            firstName: String, 
+            lastName: String, 
+            email: String, 
+            password: String
+        ): Employee
+        updateTask(
+            _id: ID!, 
+            title: String, 
+            description: String, 
+            type: String, 
+            status: String
+        ): Task
+        deleteEmployee(
+            _id: ID!
+        ): Employee
+        deleteTask(
+            _id: ID!
+        ): Task
+    }
 `;
 
 module.exports = typeDefs;
