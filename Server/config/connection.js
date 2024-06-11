ffcd5a436868e22fd18de40b54d303966321179d
Orion Cannon <orion.m.cannon@gmail.com>
2024-06-10 19:02:03 -0700

    added process_env to mongoDB

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(MONGODB_URI ||'mongodb://127.0.0.1:27017/OnlyTasks');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/OnlyTasks"
+);
 
 module.exports = mongoose.connection;
